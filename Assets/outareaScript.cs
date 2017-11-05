0ed2d1e8f40780d63a5f64e5e384fbf45dd8e1ed
tatsuyan <tatsuyan_japan@yahoo.co.jp>
2017-11-06 06:50:28 +0900

    test

---
+++ Assets/outareaScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class outareaScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider myCollider)
+    {
+        if (myCollider.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+            //SceneManager.LoadScene("自然を作成する");
+        }
+    }
+}
